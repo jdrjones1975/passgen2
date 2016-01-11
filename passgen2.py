212e80e6bea60985f3f729c554fb7497c2c4f599
JDRJ <julian.d.jones@gmail.com>
2016-01-11 12:12:22 -0600

    Test
    test

---
+++ passgen2.py
@@ -0,0 +1,140 @@
+import random
+import os
+
+ver = 0.02
+
+lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+upperCase = lowerCase.upper()
+digits = '1234567890'
+specChars = '!@#$%^&*()-=_+,.<>?/\| '
+charset = lowerCase + upperCase + digits + specChars
+
+def main():
+    introduction()
+    running = True
+    while running:
+        menu()
+    quit()
+
+def introduction():
+    print('***************************************')
+    print('* This is password generator ver', ver, '*')
+    print('*                                     *')
+    print('* Present functionality is limited    *')
+    print('* to psuedorandomness.                *')
+    print('* Future versions to be in a GUI.     *')
+    print('***************************************')
+
+def menu():
+    print()
+    print("1. Generate a random password from 1 to 64 characters.")
+    print("2. Generate a diceware password.")
+    print("3. Read the changelog.")
+    print("4. Quit")
+    selection = input("     Please make a selection(1-4): ")
+    if selection == '1':
+        randomPassword()
+    elif selection == '2':
+        diceWare()
+    elif selection == '3':
+        changelog()
+    elif selection == '4':
+        quit()
+    else:
+        print("Invalid choice")
+        return
+        
+def randomPassword():
+    length = 0
+    password = ' '
+    print("\nThis will generate a random password")
+    try:
+        length = int(input("Input your desired password length (1-64): "))
+    except ValueError:
+        print ("ValueError")
+
+    if length >= 1 and length <= 64:
+        password = ''.join(random.choice(charset) for k in range(length))
+        print()
+        print(password)
+        print()
+        copyOption(password)
+
+
+    else:
+        print("Error, returning to main menu.")
+        return
+
+
+def copyOption(element):
+    copy = input("type 'y' to copy this password to the clipboard: ")
+    if copy == 'y':
+        copyToClip(element)
+
+def copyToClip(element):
+    os.system("echo '%s' | pbcopy" % element)
+    print (element, "is in the clipboard")
+
+def diceWare():
+    '''
+    Learn more about diceware at http://world.std.com/~reinhold/diceware.html
+    '''
+    
+    print()
+    print("Diceware™ is a method for picking passphrases that uses dice to select words at random from a special list called the Diceware Word List.")
+    print()
+    
+    dic = {} #creates empty dictionary for key/values from dicewaremaster.txt
+    length = 5 #begin with 5, can make this a var as input later to determine how many words returned as password
+    try:
+        length = int(input("Input your desired number of words: "))
+    except ValueError:
+        print ("ValueError, using default of 5")
+    password = [] #initialize an empty password / word holder
+    
+    f = open('dicewaremaster.txt', 'r') #opens dicewaremaster file
+    for l in f:
+        '''
+        writes diceware dictionary to dic
+        '''
+        k, v = l.split()
+        if k in dic:
+            dic[k].extend(v)
+        else:
+            dic[k] = [v]
+
+    f.close() #closes txt file
+
+    #initialize the five dice and roll them length times
+    for i in range(0,length):
+        ones = (random.randint(1,6))
+        tens = 10 * (random.randint(1,6))
+        huns = 100 * (random.randint(1,6))
+        thous = 1000 * (random.randint(1,6))
+        tenthous = 10000 * (random.randint(1,6))
+        rawRoll = tenthous + thous + huns + tens + ones
+        rawRoll = str(rawRoll)
+        password.append(dic[rawRoll])
+
+    flatPass = list(flatten(password))
+    password = ' '.join(flatPass)
+    print()
+    print(password)
+    print()
+    #increaseentropy function option here
+    copyOption(password)
+
+
+def flatten(lst):
+    for elem in lst:
+        if type(elem) in (tuple, list):
+            for i in flatten(elem):
+                yield i
+        else:
+            yield elem
+
+def changelog():
+    print(ver, "combines random password and psuedo-diceware password into one app.")
+
+main()
+quit()
